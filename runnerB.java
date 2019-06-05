ee385b2fce812ac8703b8a42eb271361faa7d3f1
Kunal Amladi <kunalamladi@outlook.com>
2019-06-05 10:15:57 -0400

    Runner and Interface for perfect square functionality created

---
+++ runnerB.java
@@ -0,0 +1,20 @@
+public class runnerB {
+	public static void main(String[]args){
+		partnerB pB = (a)->{
+            return (a == (Math.sqrt(a)*Math.sqrt(a)));
+        };
+        System.out.println(pB.Perfect_Square(4f));
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
